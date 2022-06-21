7f09558892312de60b3ba28ad7cdc0698ac3c691
ffelixneto <ffelixneto@outlook.com>
2022-06-21 15:22:31 -0300

    Passando dados para o template

--- contas/views.py
+++ contas/views.py
@@ -7,11 +7,16 @@ from django.http import HttpResponse
 from datetime import datetime
 
 def home(request):
-    '''
+    """
     PAGNA HOME RETORNA A HORA ATUAL
-    '''
-    agora =  datetime.now()
-    html = "<html><body>Quando são exactamente... %s.</body></html>" % agora
-    # return HttpResponse(html)
+    """
 
-    return render(request, 'contas/home.html')
+    # html = "<html><body>Quando são exactamente... %s.</body></html>" % agora
+    # return HttpResponse(html)
+    
+    # DICIONARIO DE DADOS PARA RETORNO
+    page_data = {}
+
+    page_data["agora"] = datetime.now()
+    page_data["nomes"] = ["Felix", "Paulo", "Adelson"]
+    return render(request, "contas/home.html", page_data)
